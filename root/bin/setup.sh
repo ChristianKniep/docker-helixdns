e840e00d836f533ec6b268b1a6ee86e4e76791d9
Christian Kniep <christian@qnib.org>
2014-04-30 20:18:35 +0200

    advertise the etcd server (maybe not correct)

--- root/bin/setup.sh
+++ root/bin/setup.sh
@@ -11,13 +11,13 @@ MY_IP=$(ip -o -4 addr|grep eth0|awk '{print $4}'|awk -F/ '{print $1}')
 echo "MY_IP=${MY_IP}"
 echo "### Send IP to etcd"
 for HOST in $(hostname) ${ALIASES};do
-    echo "# curl -s -XPUT http://${MASTER_IP}:4001/v2/keys/helix/${HOST}/A -d value=${MY_IP}"
-    curl -s -XPUT http://${MASTER_IP}:4001/v2/keys/helix/${HOST}/A -d value="${MY_IP}"
+    echo "# curl -4 -s -XPUT http://${MASTER_IP}:4001/v2/keys/helix/${HOST}/A -d value=${MY_IP}"
+    curl -4 -s -XPUT http://${MASTER_IP}:4001/v2/keys/helix/${HOST}/A -d value="${MY_IP}"
 done
 echo "### Send PTR to etcd"
 MY_PTR=$(echo ${MY_IP}|sed -e 's#\.#/#g')
-echo "# curl -s -XPUT http://${MASTER_IP}:4001/v2/keys/helix/arpa/in-addr/${MY_PTR}/PTR -d value=$(hostname)."
-curl -s -XPUT http://${MASTER_IP}:4001/v2/keys/helix/arpa/in-addr/${MY_PTR}/PTR -d value="$(hostname)."
+echo "# curl -4 -s -XPUT http://${MASTER_IP}:4001/v2/keys/helix/arpa/in-addr/${MY_PTR}/PTR -d value=$(hostname)."
+curl -4 -s -XPUT http://${MASTER_IP}:4001/v2/keys/helix/arpa/in-addr/${MY_PTR}/PTR -d value="$(hostname)."
 
 sleep 2
 exit 0
